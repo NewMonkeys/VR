1442b804ad81ef8c9c8968b012aaedbcec073eb0
森田勝 <morisug4645@gmail.com>
2018-08-27 13:53:57 +0900

    orizin

---
+++ fire_monkey/Assets/work/abe/Script/BackDraftDoor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackDraftDoor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject decision;
+
+    //private Decision decision;
+
+    private ParticleSystem particle;
+
+    public AudioClip se;
+
+    private AudioSource audioSource;
+
+    //1回だけ処理するトリガー
+    private bool trg = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        particle = this.GetComponent<ParticleSystem>();
+
+        // ここで Particle System を停止する.
+        particle.Stop();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "door")
+        {
+            // ここで Particle System を開始します.
+            particle.Play();
+
+            if (trg == false)
+            {
+                audioSource.PlayOneShot(se);
+                decision.GetComponent<Decision>().DraftDead();
+                trg = true;
+            }
+        }
+    }
+}
+
+
---
+++ fire_monkey/Assets/work/satou/Scripts/Decision.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class Decision : MonoBehaviour {
+
+    //プレイヤー
+    [SerializeField]
+    private GameObject area;
+
+    [SerializeField]
+    private GameObject ban;
+
+    public AudioClip se;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    //ゲームクリア
+    private IEnumerator DelayMethod_C(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Clear");
+    }
+
+    //炎専用遅延処理をしてプレイヤーを移動させる関数
+    //炎による死亡
+    private IEnumerator DelayMethod_F(float waitTime)
+    {
+        area.GetComponent<VRTK_HeadsetFade>().Fade(Color.black, 3.0f);
+        yield return new WaitForSeconds(waitTime);
+
+        Change.num = 0;
+        SceneManager.LoadScene("GameOver");
+
+    }
+
+    //炎専用遅延処理をしてプレイヤーを移動させる関数
+    //煙による死亡
+    private IEnumerator DelayMethod_S(float waitTime)
+    {
+        area.GetComponent<VRTK_HeadsetFade>().Fade(Color.black, 3.0f);
+        yield return new WaitForSeconds(waitTime);
+
+        Change.num = 1;
+        SceneManager.LoadScene("GameOver");
+    }
+
+    //炎専用遅延処理をしてプレイヤーを移動させる関数
+    //煙による死亡
+    private IEnumerator DelayMethod_B(float waitTime)
+    {
+        area.GetComponent<VRTK_HeadsetFade>().Fade(Color.black, 3.0f);
+        yield return new WaitForSeconds(waitTime);
+
+        Change.num = 2;
+        SceneManager.LoadScene("GameOver");
+    }
+
+    //炎専用遅延処理をしてプレイヤーを移動させる関数
+    //チュートリアルによる死亡
+    private IEnumerator DelayMethod_T(float waitTime)
+    {
+        area.GetComponent<VRTK_HeadsetFade>().Fade(Color.black, 3.0f);
+        yield return new WaitForSeconds(waitTime);
+
+        //Change.num = 2;
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void gameOver(string damegeName)
+    {
+
+        if (damegeName == "fireWall")
+        {
+            //ワープ禁止エリアを召喚
+            audioSource.PlayOneShot(se);
+            Instantiate(ban,transform.position,Quaternion.identity);
+            Coroutine coroutine = StartCoroutine("DelayMethod_F", 3.0f);
+        }
+    }
+
+    public void SmokeDead()
+    {
+        //ワープ禁止エリアを召喚
+        audioSource.PlayOneShot(se);
+        Instantiate(ban, transform.position, Quaternion.identity);
+        Coroutine coroutine = StartCoroutine("DelayMethod_S", 3.0f);
+    }
+
+    public void DraftDead()
+    {
+        //ワープ禁止エリアを召喚
+        audioSource.PlayOneShot(se);
+        Instantiate(ban, transform.position, Quaternion.identity);
+        Coroutine coroutine = StartCoroutine("DelayMethod_B", 3.0f);
+    }
+
+    public void TutorialDead()
+    {
+        //ワープ禁止エリアを召喚
+        audioSource.PlayOneShot(se);
+        Instantiate(ban, transform.position, Quaternion.identity);
+        Coroutine coroutine = StartCoroutine("DelayMethod_T", 3.0f);
+    }
+
+    public void gameClear()
+    {
+        area.GetComponent<VRTK_HeadsetFade>().Fade(Color.white, 2.0f);
+        Debug.Log("Clear");
+
+        //遅延をしてドームに移動させる
+
+        //ワープ禁止エリアを召喚
+        Instantiate(ban, transform.position, Quaternion.identity);
+        Coroutine coroutine = StartCoroutine("DelayMethod_C", 2.0f);
+    }
+
+
+}
