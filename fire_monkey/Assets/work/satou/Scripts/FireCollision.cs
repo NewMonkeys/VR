3dea9d6cf1eff6f7a182cf1deeceef963cf136cb
DA\nwuser <goroumakisimamu810@gmail.com>
2018-10-18 15:33:39 +0900

    消火器のサンプルシーンにて火を消せるようになった

---
+++ fire_monkey/Assets/work/satou/Scripts/FireCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "fireWall")
+        {
+            Debug.Log("ジュージュー");
+            Destroy(other.gameObject);
+        }
+    }
+
+    /*
+    private void OnParticleTrigger(GameObject other)
+    {
+        if (other.tag == "fireWall")
+        {
+            Debug.Log("ジュージュー");
+            Destroy(other.gameObject);
+        }
+    }*/
+}
